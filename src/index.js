b0272b73aee2b62d72dc4cb829f630806e568ac1
Yndrina del valle <yndrinabarrios@gmail.com>
2023-05-17 10:19:23 -0500

    eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import validator from './validator.js';
 
-console.log(validator);
+const tarjeta = document.querySelector('#tarjeta');
+
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
